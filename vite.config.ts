cdb8fff7ff976947262a6fa4a4b000c08cdb8805
Ali000Hasan <ah630603@gmail.com>
2026-09-08 10:53:23 -0700

    Finish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' 
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), 
+  ],
   base: '/Task-6-Adv/',
 })
